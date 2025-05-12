0e54e7b8501020f24e735340c0a1253567d0f634
Patobeur <patobeur41@gmail.com>
2025-05-12 09:51:08 +0200

    new file:   accords.js 	new file:   index.html 	new file:   main.js 	new file:   style.css

---
+++ accords.js
@@ -0,0 +1,155 @@
+let lesAccords = [
+    {
+        "accord": "C",
+        "type": "majeur",
+        "note": "C",
+        "tab": [null,3,2,0,1,0],
+        "barre": null,
+        "fingers": [null,3,2,null,1,null],
+        "famille": {
+            "nom":"Triade",
+            "structure":{
+                "NoteFondamentale": "C",
+                "TierceMajeure": "E",
+                "QuinteJuste": "G"
+            }
+        }
+    },
+    {
+        "accord": "F",
+        "type": "majeur",
+        "note": "F",
+        "tab": [1,3,3,2,1,1],
+        "barre": {"fret": 1,"from": 6,"to": 1},
+        "fingers": [1,3,4,2,null,null],
+        "famille": {
+            "nom":"Triade",
+            "structure":{
+                "NoteFondamentale": "F",
+                "TierceMajeure": "A",
+                "QuinteJuste": "C"
+            }
+        }
+    },
+    {
+        "accord": "D",
+        "type": "majeur",
+        "note": "D",
+        "tab": [null,null,0,2,3,2],
+        "barre": null,
+        "fingers": [null,null,null,1,3,2],
+        "famille": {
+            "nom":"Triade",
+            "structure":{
+                "NoteFondamentale": "D",
+                "TierceMajeure": "F#",
+                "QuinteJuste": "A"
+            }
+        }
+    },
+    {
+        "accord": "E",
+        "type": "majeur",
+        "note": "E",
+        "tab": [0,2,2,1,0,0],
+        "fingers": [null,2,3,1,null,null],
+        "famille": {
+            "nom":"Triade",
+            "structure":{
+                "NoteFondamentale": "E",
+                "TierceMajeure": "G#",
+                "QuinteJuste": "B"
+            }
+        }
+    },
+    {
+        "accord": "G",
+        "type": "majeur",
+        "note": "G",
+        "fr": "sol",
+        "tab": [3,2,0,0,0,3],
+        "fingers": [2,1,null,null,null,4],
+        "famille": {
+            "nom":"Triade",
+            "structure":{
+                "NoteFondamentale": "G",
+                "TierceMajeure": "B",
+                "QuinteJuste": "D"
+            }
+        }
+    },
+    {
+        "accord": "A",
+        "type": "majeur",
+        "note": "A",
+        "fr": "la",
+        "tab": [null,0,2,2,2,0],
+        "famille": {
+            "nom":"Triade",
+            "structure":{
+                "NoteFondamentale": "A",
+                "TierceMajeure": "C#",
+                "QuinteJuste": "E"
+            }
+        }
+    },
+    {
+        "accord": "B",
+        "type": "majeur",
+        "note": "B",
+        "tab": [null,2,4,4,4,2],
+        "barre": {"fret": 2,"from": 5,"to": 1},
+        "famille": {
+            "nom":"Triade",
+            "structure":{
+                "NoteFondamentale": "B",
+                "TierceMajeure": "D#",
+                "QuinteJuste": "F#"
+            }
+        }
+    },
+    {
+        "accord": "Dm",
+        "type": "mineur",
+        "note": "D",
+        "fr": "ré",
+        "tab": [
+            null,
+            null,
+            0,
+            2,
+            3,
+            1
+        ],
+        "famille": {
+            "nom":"Triade",
+            "structure":{
+                "NoteFondamentale": "D",
+                "TierceMineure": "F",
+                "QuinteJuste": "A"
+            }
+        }
+    },
+    {
+        "accord": "Em",
+        "type": "mineur",
+        "note": "E",
+        "fr": "mi",
+        "tab": [
+            0,
+            2,
+            2,
+            0,
+            0,
+            0
+        ],
+        "famille": {
+            "nom":"Triade",
+            "structure":{
+                "NoteFondamentale": "E",
+                "TierceMineure": "G",
+                "QuinteJuste": "B"
+            }
+        }
+    },
+]
---
+++ main.js
@@ -0,0 +1,383 @@
+"use strict";
+function deleteChord() {
+  if (modal.index !== null) {
+    const confirmDelete = confirm("Voulez-vous vraiment supprimer cet accord ?");
+    if (confirmDelete) {
+      jsonManager.datas.splice(modal.index, 1);
+      localStorage.setItem("accords", JSON.stringify(jsonManager.datas));
+      modal.closeModal();
+      location.reload();
+    }
+  }
+}
+
+const rules = {
+	version: 0.01,
+	notes: {
+		us: ['A','B','C','D','E','F','G'],
+		fr: ['La','Si','Do','Ré','Mi','Fa','Sol']
+	},
+	familles: [], // futures categories d'accords ?
+	fr2us:function(note){
+		return this.notes.us[this.notes.fr.indexOf(note)];
+	},
+	us2fr:function(note){
+		return this.notes.fr[this.notes.us.indexOf(note)];
+	},
+}
+const displaySize = {sizeDiv:document.createElement('div'),init:function(){this.sizeDiv.className='screensize';document.body.append(this.sizeDiv);this.sizeDiv.textContent=device.device+':'+window.innerWidth+' x '+window.innerHeight;window.addEventListener("resize",(event)=>{this.sizeDiv.textContent= device.device+':'+window.innerWidth+' x '+window.innerHeight});}}
+const jsonManager = {
+	fileNamePath:"accords.json",
+	dynamique:true,// localstorage activé
+	datas:null,
+	storedDatas:null,
+	json:null,
+	// set:function(datasChords){
+	// 	this.datas = datasChords
+	// 	// this.json = JSON.stringify(this.datas)
+	// 	this.saveDatas()
+	// },
+	saveDatas:function(){
+		this.json = JSON.stringify(this.datas)
+		localStorage.setItem("accords", this.json);
+	},
+	init:function(){
+		// STORAGE
+		this.storedDatas = localStorage.getItem("accords")
+
+		if (this.dynamique && this.storedDatas) {
+			
+			this.datas = JSON.parse(this.storedDatas)
+			displayManager.start()
+		} else {
+			
+				this.datas = lesAccords
+				this.saveDatas()
+				displayManager.start()
+
+			// fetch(this.fileNamePath)
+			// .then(response => response.json())
+			// .then(datasChords => {
+			// 	this.datas = datasChords
+			// 	this.saveDatas()
+			// 	displayManager.start()
+			// });
+		}
+	},
+	exportJSON:function(){
+		const blob = new Blob([JSON.stringify(this.datas, null, 2)], { type: 'application/json' });
+		const url = URL.createObjectURL(blob);
+		const a = document.createElement('a');
+		a.href = url;
+		a.download = 'accords.json';
+		a.click();
+		// on vide url
+		URL.revokeObjectURL(url);
+	},
+	importJSON:function(event){
+		const file = event.target.files[0];
+		if (!file) return;
+
+		const reader = new FileReader();
+		reader.onload = (e) =>{
+			try {
+				const imported = JSON.parse(e.target.result);
+				if (Array.isArray(imported)) {
+					console.log(imported)
+					this.datas = imported;
+					console.log(this.datas)
+					this.saveToLocalStorage();
+					location.reload();
+				} else {
+					alert('Fichier JSON invalide.');
+				}
+			} catch (err) {
+				alert(err);
+			}
+		};
+		reader.readAsText(file);
+	},
+	saveToLocalStorage:function() {
+		console.log('saveToLocalStorage')
+		localStorage.setItem("accords", JSON.stringify(this.datas));
+	}
+}
+const svgManager = {
+	createChordSVG:function(item){
+		if (item.tab) {
+			let fretStart = (item.barre && item.barre.fret) ? item.barre.fret : 1;
+			const svgNS = "http://www.w3.org/2000/svg";
+			const svg = document.createElementNS(svgNS, "svg");
+			svg.setAttribute("viewBox", "0 0 60 60");
+			// Le dessin du barré si y'en a un
+			if (item.barre && item.barre.fret && fretStart && item.barre.from && item.barre.to) {
+				const barreY = 10 + (item.barre.fret - fretStart) * 10 + 5;
+				const xStart = 10 + (6 - item.barre.from) * 8;
+				const xEnd = 10 + (6 - item.barre.to) * 8;
+				const rect = document.createElementNS(svgNS, "rect");
+				rect.setAttribute("y", barreY - 3);
+				rect.setAttribute("x", Math.min(xStart, xEnd) - 3); // décale vers la gauche
+				rect.setAttribute("width", Math.abs(xEnd - xStart) + 6); // augmente la largeur
+				rect.setAttribute("height", 6);
+				rect.setAttribute("fill", "black");
+				rect.setAttribute("rx", 3);  // rayon horizontal
+				rect.setAttribute("ry", 3);  // rayon vertical
+				svg.appendChild(rect);
+			}
+			// Affiche le numéro de frette si > 1
+			if (fretStart >= 0) {
+				const text = document.createElementNS(svgNS, "text");
+				text.setAttribute("x", 2);
+				text.setAttribute("y", 17);
+				text.setAttribute("font-size", "6");
+				text.textContent = fretStart;
+				svg.appendChild(text);
+			}
+			// Marqueurs au-dessus des cordes (X ou O)
+			item.tab.forEach((fret, i) => {
+				const x = 10 + i * 8;
+				const y = 8;
+				const marker = document.createElementNS(svgNS, "text");
+				marker.setAttribute("x", x - 2);
+				marker.setAttribute("y", y);
+				marker.setAttribute("font-size", "5");
+				if (fret === null) {
+					marker.textContent = "x";
+				} else if (fret === 0) {
+					marker.textContent = "o";
+				}
+				if (marker.textContent) svg.appendChild(marker);
+			});
+			// Les Cordes (verticales)
+			for (let i = 0; i < 6; i++) {
+				const line = document.createElementNS(svgNS, "line");
+				line.setAttribute("x1", 10 + i * 8);
+				line.setAttribute("y1", 10);
+				line.setAttribute("x2", 10 + i * 8);
+				line.setAttribute("y2", 60);
+				line.setAttribute("stroke", "black");
+				svg.appendChild(line);
+			}
+			// les Frettes (horizontales)
+			for (let i = 0; i < 5; i++) {
+				const line = document.createElementNS(svgNS, "line");
+				line.setAttribute("x1", 10);
+				line.setAttribute("y1", 10 + i * 10);
+				line.setAttribute("x2", 10 + 5 * 8);
+				line.setAttribute("y2", 10 + i * 10);
+				line.setAttribute("stroke", "black");
+				svg.appendChild(line);
+			}
+			// les points pour les doigts
+			item.tab.forEach((fret, i) => {
+				if (fret !== null && fret !== 'null' && fret > 0) {
+					const y = 10 + (fret - fretStart) * 10 + 5;
+					const x = 10 + i * 8;
+					
+					const circle = document.createElementNS(svgNS, "circle");
+					circle.setAttribute("cx", x);
+					circle.setAttribute("cy", y);
+					circle.setAttribute("r", 3);
+					circle.setAttribute("fill", "black");
+					svg.appendChild(circle);
+
+					// le numéro des doigts
+					if(item.fingers){
+						const numDoigt = document.createElementNS(svgNS, "text");
+						numDoigt.setAttribute("x", x - 1.5);
+						numDoigt.setAttribute("y", y + 2);
+						numDoigt.setAttribute("font-size", "5");
+						numDoigt.setAttribute("fill", "white");
+						let doig = item.fingers[i]
+							numDoigt.textContent = (doig!='null' && doig!=null) ? item.fingers[i] : '';
+							svg.appendChild(numDoigt);
+						}
+				}
+			});
+			return svg;
+		}
+		return false;
+	}
+}
+const displayManager = {
+	cardsDiv:document.createElement('div'),
+	init:function(){
+		device.detect()
+		jsonManager.init()
+		modal.init()
+	},
+	start: function(){
+		this.displaycardsDiv(jsonManager.datas)
+		displaySize.init()
+	},
+	displaycardsDiv: function(){
+		this.cardsDiv.className = 'cards';
+		let i = 0;
+		jsonManager.datas.forEach(item => {
+			this.creatChordCard(item, i);
+			i++;
+		});
+		document.body.appendChild(this.cardsDiv);
+	},
+	creatChordCard:function(item,i){
+		const card = document.createElement('div');
+		card.className = 'card '+ item.famille.nom;
+		card.addEventListener('click', (e) => {modal.openModal(i)})
+		
+		const divContent = document.createElement('div');
+		divContent.className = 'card-content';
+		card.appendChild(divContent);
+
+		const family = document.createElement('div');
+		family.className = 'family';
+		family.textContent = item.famille.nom;
+		divContent.appendChild(family);
+
+		const chord = document.createElement('div');
+		chord.className = 'chord';
+		chord.textContent = item.accord;
+		divContent.appendChild(chord);
+
+		const noteFR = document.createElement('div');
+		noteFR.className = 'note-fr';
+		noteFR.textContent = rules.us2fr(item.accord[0]);
+		divContent.appendChild(noteFR);
+
+		if (item.tab) {
+			let fretStart = 1;
+			if (item.barre && item.barre.fret) {
+				fretStart = item.barre.fret;
+			}
+			const svg = svgManager.createChordSVG(item);
+			if(svg) divContent.appendChild(svg);
+		}
+
+		if(item.famille) {
+			if(item.famille.structure) {
+				const chordsfamille = document.createElement('div');
+				chordsfamille.className = 'chordsfamille';
+				let blocs = item.famille.structure
+				
+				let i = 0
+				Object.entries(blocs).forEach(entry => {
+					const [key, value] = entry;
+					i++;
+					let tempoDiv = document.createElement('div');
+					tempoDiv.className = 'note_'+i;
+					tempoDiv.textContent = value;
+					tempoDiv.title = key;
+					chordsfamille.appendChild(tempoDiv);
+				});
+				divContent.appendChild(chordsfamille);
+			}
+		}
+		displayManager.cardsDiv.appendChild(card);
+	},
+	areChordsEqual:function(a, b) {
+		const sameTab = JSON.stringify(a.tab) === JSON.stringify(b.tab);
+		// const sameFret = a.fret_start === b.fret_start;
+		const sameBarre = JSON.stringify(a.barre || {}) === JSON.stringify(b.barre || {});
+		// return sameTab && sameFret && sameBarre;
+		// return sameTab && sameBarre;
+		if(sameTab){
+			console.log(a.tab)
+			console.log(b.tab)
+			console.log('------')
+		}
+		return sameTab;
+	}
+}
+const modal = {
+	index:null,
+	form:null,
+	modal:null,
+	formSubmit:function(e){
+		const form = e.target;
+		const newChord = {
+			famille: {
+				"nom":form.famille.value,
+				"structure":{
+					"NoteFondamentale": "F",
+					"TierceMajeure": "A",
+					"QuinteJuste": "C"
+				}
+			},
+			accord: form.accord.value,
+			type: form.type.value,
+			// note: form.note.value,
+			// fr: form.fr.value,
+			tab: form.tab.value.split(',').map(x => (x.trim() === 'null' || x.trim() === '') ? 'null' : parseInt(x.trim())),
+			fingers: form.fingers.value.split(',').map(x => (x.trim() === 'null' || x.trim() === '') ? 'null' : parseInt(x.trim())),
+			// fret_start: parseInt(form.fret_start.value) || 1
+		}
+		if (form.barre.value) {
+			const [fret, from, to] = form.barre.value.split('-').map(x => parseInt(x));
+			newChord.barre = { fret, from, to };
+		}
+		if ( jsonManager.datas.some(
+			(item, index) => {
+				index !== this.index && displayManager.areChordsEqual(item, newChord);
+			})
+		) {
+			alert("Cet accord existe déjà !");
+			return;
+		}
+		if (this.index !== null) {
+			console.log('modChord',newChord)
+			jsonManager.datas[this.index] = newChord;
+		} else {
+			console.log('newChord',newChord)
+			jsonManager.datas.push(newChord);
+		}
+		jsonManager.saveDatas();
+		// jsonManager.init();
+		location.reload();
+		this.closeModal();
+	},
+	openModal:function(index){
+    	this.index = index ?? null;
+		console.log('openModal',this.index)
+		console.log('openModal',this.index)
+
+		if (this.index !== null) {
+			const chord = jsonManager.datas[this.index];
+		console.log('chord',chord)
+			this.form.famille.value = chord.famille.nom;
+			this.form.famille.structure = chord.famille.structure;
+			this.form.accord.value = chord.accord;
+			this.form.type.value = chord.type;
+			// this.form.note.value = chord.note;
+			// this.form.fr.value = chord.fr;
+			this.form.tab.value = chord.tab ? chord.tab.join(',') : '0,0,0,0,0,0';
+			this.form.fingers.value = chord.fingers ? chord.fingers.join(',') : '0,0,0,0,0,0';
+			// this.form.fret_start.value = chord.fret_start || 1;
+			this.form.barre.value = chord.barre ? `${chord.barre.fret}-${chord.barre.from}-${chord.barre.to}` : '';
+		}
+		else {
+			this.form.reset();
+		}
+    	this.modal.style.display = 'flex';
+	},
+	closeModal:function() {
+		this.index = null
+		this.form.reset()
+		this.modal.style.display = 'none'
+	},
+	init:function(){
+		this.form = document.getElementById('editForm');
+		this.modal = document.getElementById('modal');
+		this.modal.addEventListener('submit',(e)=> {
+			e.preventDefault();
+			this.formSubmit(e);
+		});
+	}
+}
+const device = {
+	device:'desktop',
+	detect:function(){
+		this.device = (/Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent)) ? 'mobile' : 'desktop';
+		document.body.classList.add(this.device)
+		return this.device
+	}
+}
+window.addEventListener("load",()=>displayManager.init());
